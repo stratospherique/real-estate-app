46907c3ea3e533e45eae62e0b8c73d6619af6c64
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-12-27 13:42:55 +0100

    stickler

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
-    "parser": "@typescript-eslint/parser",
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
